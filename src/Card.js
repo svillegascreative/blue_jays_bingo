282eeb3782e966296a5553ad6bccd733f6ca20d0
sylvia villegas <sylvia@svillegas.com>
2017-07-10 13:38:07 -0400

    Create Card component & CSS

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Card.css';
+import Box from './Box.js';
+
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default Card;
