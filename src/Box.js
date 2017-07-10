84477ebb611db5af39e3e960c48ab3b71187a328
sylvia villegas <sylvia@svillegas.com>
2017-07-10 13:35:58 -0400

    Create Box component & CSS

---
+++ src/Box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Box.css';
+
+class Box extends Component {
+  render() {
+    return (
+      <div className="Box">
+      </div>
+    );
+  }
+}
+
+export default Box;
