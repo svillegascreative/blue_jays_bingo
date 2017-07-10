2be8cb8425343bbc49255fd20567128e8b5a82a7
sylvia villegas <sylvia@svillegas.com>
2017-07-10 13:40:14 -0400

    Add Card to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Card.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Blue Jays Broadcast Bingo</h1>
+        <Card />
       </div>
     );
   }
