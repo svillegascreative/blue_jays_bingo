8fb085860d0092464eb22de01dc355ed406c079f
sylvia villegas <sylvia@svillegas.com>
2017-07-10 13:42:32 -0400

    Create events array

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './Card.js';
 
+const events =  [
+    "Buck says 'Get up ball!'",
+    "Pat says 'pounding the strike zone'",
+    "Anyone is 'really hitting the ball well'",
+    "Anyone is 'really seeing the ball well'",
+    "A pitcher's 'got great stuff'",
+    "Hazel reports from the side of the field",
+    "Arash reports from the side of the field",
+    "Any broadcaster tells how they 'talked to him before the game'"
+  ]
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Blue Jays Broadcast Bingo</h1>
-        <Card />
       </div>
     );
   }
